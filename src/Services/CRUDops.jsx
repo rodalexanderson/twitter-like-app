fe50e3e937764ea8c368bf3abc78537a3ae52f8c
rodalexanderson <alexanderson.ibarra@gmail.com>
2021-12-29 15:50:51 -0600

    se agregan componentes

---
+++ src/Services/CRUDops.jsx
@@ -0,0 +1,41 @@
+import { db } from './firebase';
+import {collection, doc, getDocs, addDoc, updateDoc, onSnapshot} from "firebase/firestore"
+
+// Funcion helper
+const getDocRef = (col, id) => doc(db, col, id)
+const getCollectionRef = (col) => collection(db, col )
+
+// Get data
+export const getData = async (col) =>{
+    collectionRef = getCollectionRef(col)
+    const snapData = await getDocs(collectionRef)
+    const data = snapData.map(doc => doc.data())
+    return data
+}
+
+// Set data
+export const setData = async (col, data) =>{
+    collectionRef = getCollectionRef(col)
+    // Se crea un nuevo docuento que se llena con collectionRef y data. Como es promesa, se espera con await
+    const docRef = await addDoc(collectionRef, data)
+    return docRef
+}
+
+// Update data
+export const updateData = async (col, id, data) =>{
+    docRef = getDocRef(col, id);
+    await updateDoc(docRef, data)
+}
+
+// Delete data 
+export const deleteData = async (col, id) =>{
+    docRef = getDocRef(col, id);
+    await deleteteDoc(docRef)
+}
+
+// Suscribe data
+export const suscribetData = async (col, callback) =>{
+    collectionRef = getCollectionRef(col)
+    const unsuscribe = onSnapshot(collectionRef, callback);
+    return unsuscribe
+}
---
+++ src/Services/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCB6l4O3HzzZ3uMHseKjS7L-X2voEFdRSo",
+  authDomain: "twitterapp-aad25.firebaseapp.com",
+  projectId: "twitterapp-aad25",
+  storageBucket: "twitterapp-aad25.appspot.com",
+  messagingSenderId: "871128450978",
+  appId: "1:871128450978:web:25893a5eb84d3b4cbc94fb"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
