e609d746fede2a5e8f893a7080266c8229e119f7
rodalexanderson <alexanderson.ibarra@gmail.com>
2022-01-21 21:50:17 -0600

    se agregan archivos multimedia, tipografia, se modifica el css de sign in

--- src/Components/SignInGoogle/index.jsx
+++ src/Components/SignInGoogle/index.jsx
@@ -1,10 +1,29 @@
 import { signInGoogle } from "../../Services/authGoogle"
+import Logo from "../../multimedia/SVG/logo.svg"
+import Google from "../../multimedia/png/google.png"
 
 const SignInGoogle = () => {
     return( 
-        <button onClick={signInGoogle}>
-            Sign In With Google
-        </button>
+        <>
+        <div className="signin">
+            <div className="leftsidesignin">
+                <img src={Logo} alt="Logo Devs United" className="logo"/>
+            </div>
+            <div className="rightsidesignin">
+                <div className="rightsigninmaxwidth">
+                    <h1 className="h1signin">Lorem, ipsum dolor.</h1>
+                    <h2 className="h2signin">Lorem ipsum dolor sit amet consectetur adipisicing elit</h2>
+                    <div className="button">
+                        <img src={Google} alt="google icon" className="google" />
+                        <button onClick={signInGoogle}>
+                            Sign In With Google
+                        </button>
+                    </div>
+                    <h4>© 2020 Devs_United - <span className="spanred"><b>BETA</b> </span> </h4>
+                </div>   
+            </div>
+        </div>
+        </>
     )
 }
 
